f4f281d97887a3bf19381067059ae2caf6638b5f
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-10-22 11:22:08 +0700

    ep11

--- src/app/(demo)/examples/dashboard/page.tsx
+++ src/app/(demo)/examples/dashboard/page.tsx
@@ -3,14 +3,21 @@
 import { useRouter } from "next/navigation";
 import React from "react";
 
-type Props = {};
+type Props = {
+  searchParams: {
+    id?: string;
+    type?: string;
+  };
+};
 
-export default function Dashboard({}: Props) {
+export default function Dashboard({ searchParams }: Props) {
   const router = useRouter();
 
   return (
     <div>
       <b>Dashboard</b>
+      <br />
+      id: {searchParams.id}, type: {searchParams.type}
       <br />
       <button onClick={() => router.push("/examples/aboutus")}>ABOUT US</button>
     </div>
