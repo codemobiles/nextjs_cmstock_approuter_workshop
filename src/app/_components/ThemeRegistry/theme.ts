ed6482bda0a8627a8da3ae6d77745472546af673
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-10-23 12:06:09 +0700

    ep26

--- src/app/_components/ThemeRegistry/theme.ts
+++ src/app/_components/ThemeRegistry/theme.ts
@@ -33,7 +33,7 @@ const theme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
   },
-  spacing: 20,
+  spacing: 8,
   palette: {
     mode: "light",
     primary: process.env.NEXT_PUBLIC_IS_PRODUCTION == "1" ? red : blue,
