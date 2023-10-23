dd9f8fe33a1f2e6154688ca6e749d0fbff54f930
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-10-23 10:01:04 +0700

    ep19

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -5,6 +5,28 @@ import Box from "@mui/material/Box";
 import Header from "../_components/layout/Header";
 import Sidebar from "../_components/layout/Sidebar";
 import DrawerHeader from "../_components/layout/DrawerHeader";
+import { styled } from "@mui/material";
+
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
 
 type Props = { children: React.ReactNode };
 
@@ -28,8 +50,10 @@ export default function DefaultLayout({
         <Sidebar open={open} handleDrawerClose={handleDrawerClose} />
 
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <DrawerHeader />
-          {children}
+          <Main open={open}>
+            <DrawerHeader />
+            {children}
+          </Main>
         </Box>
       </Box>
     </section>
--- src/app/_components/layout/Sidebar.tsx
+++ src/app/_components/layout/Sidebar.tsx
@@ -17,45 +17,9 @@ import MailIcon from "@mui/icons-material/Mail";
 
 const drawerWidth = 240;
 
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create("margin", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
-
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean;
 }
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(["margin", "width"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
