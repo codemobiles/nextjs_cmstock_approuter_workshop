f5e41d692bff15a4e4bbdd14168f5e90b436f4b5
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-10-23 10:56:24 +0700

    ep23

--- src/app/(routes)/stock/page.tsx
+++ src/app/(routes)/stock/page.tsx
@@ -1,8 +1,20 @@
 import { Box } from "@mui/material";
 import React from "react";
+import Image from "next/image";
 
 type Props = {};
 
 export default function Stock({}: Props) {
-  return <Box sx={{ mt: 1 }}>Stock</Box>;
+  return (
+    <Box sx={{ mt: 1 }}>
+      Stock
+      <Image
+        src="/static/img/next_login.jpg"
+        width={180}
+        height={35}
+        alt="logo"
+        style={{ objectFit: "contain" }}
+      />
+    </Box>
+  );
 }
