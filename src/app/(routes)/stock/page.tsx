46574c69f6e347fe34e168eee97bdb726f830ddc
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-10-25 18:12:52 +0700

    ep65

--- src/app/(routes)/stock/page.tsx
+++ src/app/(routes)/stock/page.tsx
@@ -11,31 +11,52 @@ import { getProducts, productSelector } from "@/src/store/slices/productSlice";
 import { useAppDispatch } from "@/src/store/store";
 import Image from "next/image";
 import { productImageURL } from "@/src/utils/commonUtil";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
+import { Typography } from "@mui/material";
+import { NumericFormat } from "react-number-format";
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
+  { field: "id", headerName: "ID", width: 90 },
   {
     field: "image",
     headerName: "Image",
     width: 70,
     renderCell: ({ value }: GridRenderCellParams<String>) => (
-      <Image
-        key={value}
-        height={500}
-        width={500}
-        alt="product image"
-        src={productImageURL(value)}
-        style={{
-          width: 70,
-          height: 70,
-          borderRadius: "5%",
-          objectFit: "cover",
-        }}
-      />
+      <Zoom>
+        <Image
+          key={value}
+          height={500}
+          width={500}
+          alt="product image"
+          src={productImageURL(value)}
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: "5%",
+            objectFit: "cover",
+          }}
+        />
+      </Zoom>
     ),
   },
-  { field: "name", headerName: "Name", width: 130 },
-  { field: "price", headerName: "Price", width: 130 },
+  { field: "name", headerName: "Name", width: 350 },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 130,
+    renderCell: ({ value }: GridRenderCellParams<String>) => (
+      <Typography variant="body1">
+        <NumericFormat
+          value={value}
+          displayType={"text"}
+          thousandSeparator={true}
+          decimalScale={0}
+          fixedDecimalScale={true}
+        />
+      </Typography>
+    ),
+  },
   { field: "stock", headerName: "Stock", width: 130 },
 ];
 
