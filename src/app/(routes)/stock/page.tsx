f11290d452af25251c6f25b25c9cdf22f36052a5
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-10-26 14:26:07 +0700

    ep69

--- src/app/(routes)/stock/page.tsx
+++ src/app/(routes)/stock/page.tsx
@@ -21,89 +21,90 @@ import dayjs from "dayjs";
 import { Add, Delete, Edit } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
 
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "image",
-    headerName: "Image",
-    width: 70,
-    renderCell: ({ value }: GridRenderCellParams<String>) => (
-      <Zoom>
-        <Image
-          key={value}
-          height={500}
-          width={500}
-          alt="product image"
-          src={productImageURL(value)}
-          style={{
-            width: 70,
-            height: 70,
-            borderRadius: "5%",
-            objectFit: "cover",
-          }}
-        />
-      </Zoom>
-    ),
-  },
-  { field: "name", headerName: "Name", width: 350 },
-  {
-    field: "price",
-    headerName: "Price",
-    width: 130,
-    renderCell: ({ value }: GridRenderCellParams<String>) => (
-      <Typography variant="body1">
-        <NumericFormat
-          value={value}
-          displayType={"text"}
-          thousandSeparator={true}
-          decimalScale={0}
-          fixedDecimalScale={true}
-        />
-      </Typography>
-    ),
-  },
-  { field: "stock", headerName: "Stock", width: 130 },
-  {
-    field: "createdAt",
-    headerName: "Timestamp",
-    width: 230,
-    renderCell: ({ value }) => (
-      <Typography variant="body1">
-        {dayjs(value).format("DD/MM/YYYY HH:mm")}
-      </Typography>
-    ),
-  },
-  {
-    headerName: "ACTION",
-    field: ".",
-    width: 120,
-    renderCell: ({ row }: GridRenderCellParams<any>) => (
-      <Stack direction="row">
-        <IconButton
-          aria-label="edit"
-          size="large"
-          onClick={() => router.push(`/stock/edit?id=${row.id}`)}
-        >
-          <Edit fontSize="inherit" />
-        </IconButton>
-        <IconButton
-          aria-label="delete"
-          size="large"
-          onClick={() => {
-            // setSelectedProduct(row);
-            // setOpenDialog(true);
-          }}
-        >
-          <Delete fontSize="inherit" />
-        </IconButton>
-      </Stack>
-    ),
-  },
-];
-
 export default function Stock() {
   const productReducer = useSelector(productSelector);
   const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "image",
+      headerName: "Image",
+      width: 70,
+      renderCell: ({ value }: GridRenderCellParams<String>) => (
+        <Zoom>
+          <Image
+            key={value}
+            height={500}
+            width={500}
+            alt="product image"
+            src={productImageURL(value)}
+            style={{
+              width: 70,
+              height: 70,
+              borderRadius: "5%",
+              objectFit: "cover",
+            }}
+          />
+        </Zoom>
+      ),
+    },
+    { field: "name", headerName: "Name", width: 350 },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 130,
+      renderCell: ({ value }: GridRenderCellParams<String>) => (
+        <Typography variant="body1">
+          <NumericFormat
+            value={value}
+            displayType={"text"}
+            thousandSeparator={true}
+            decimalScale={0}
+            fixedDecimalScale={true}
+          />
+        </Typography>
+      ),
+    },
+    { field: "stock", headerName: "Stock", width: 130 },
+    {
+      field: "createdAt",
+      headerName: "Timestamp",
+      width: 230,
+      renderCell: ({ value }) => (
+        <Typography variant="body1">
+          {dayjs(value).format("DD/MM/YYYY HH:mm")}
+        </Typography>
+      ),
+    },
+    {
+      headerName: "ACTION",
+      field: ".",
+      width: 120,
+      renderCell: ({ row }: GridRenderCellParams<any>) => (
+        <Stack direction="row">
+          <IconButton
+            aria-label="edit"
+            size="large"
+            onClick={() => router.push(`/stock/edit?id=${row.id}`)}
+          >
+            <Edit fontSize="inherit" />
+          </IconButton>
+          <IconButton
+            aria-label="delete"
+            size="large"
+            onClick={() => {
+              // setSelectedProduct(row);
+              // setOpenDialog(true);
+            }}
+          >
+            <Delete fontSize="inherit" />
+          </IconButton>
+        </Stack>
+      ),
+    },
+  ];
 
   React.useEffect(() => {
     dispatch(getProducts());
