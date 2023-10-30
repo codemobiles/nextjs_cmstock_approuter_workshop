8d51c9cc1f3ea04215f99a854393cefb4116ecfe
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-10-30 11:55:33 +0700

    ep71

--- src/app/(routes)/stock/add/page.tsx
+++ src/app/(routes)/stock/add/page.tsx
@@ -13,9 +13,27 @@ import { ProductData } from "@/src/models/product.model";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
+const formValidateSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required").trim(),
+  price: Yup.number().min(100, "Number must be greater than 100"),
+  stock: Yup.number().min(100, "Number must be greater than 100"),
+});
+
 export default function StockCreate() {
   const dispatch = useAppDispatch();
   const router = useRouter();
+  const initialValue: ProductData = { name: "", price: 1500, stock: 9999 };
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<ProductData>({
+    defaultValues: initialValue,
+    //@ts-ignore
+    resolver: yupResolver(formValidateSchema),
+  });
 
   const onSubmit = async (values: ProductData) => {
     alert(JSON.stringify(values));
@@ -28,35 +46,62 @@ export default function StockCreate() {
   };
 
   return (
-    <form noValidate onSubmit={() => {}}>
+    <form noValidate onSubmit={handleSubmit(onSubmit)}>
       <Card>
         <CardContent className="p-8">
           <Typography gutterBottom variant="h3">
             Create Product
           </Typography>
 
-          <TextField
-            label="Name"
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            autoFocus
+          <Controller
+            control={control}
+            name="name"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Name"
+                error={Boolean(errors.name?.message)}
+                helperText={errors.name?.message?.toString()}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                autoFocus
+              />
+            )}
           />
 
-          <TextField
-            label="Price"
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            autoFocus
+          <Controller
+            control={control}
+            name="price"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Price"
+                error={Boolean(errors.price?.message)}
+                helperText={errors.price?.message?.toString()}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                autoFocus
+              />
+            )}
           />
 
-          <TextField
-            label="Stock"
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            autoFocus
+          <Controller
+            control={control}
+            name="stock"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Stock"
+                error={Boolean(errors.stock?.message)}
+                helperText={errors.stock?.message?.toString()}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                autoFocus
+              />
+            )}
           />
 
           <Box>{showPreviewImage()}</Box>
