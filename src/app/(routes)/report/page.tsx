d2d3127a9866c082fe06ac0b3356385a49065f19
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-03 21:23:58 +0700

    ep 82

--- src/app/(routes)/report/page.tsx
+++ src/app/(routes)/report/page.tsx
@@ -1,7 +1,60 @@
+"use client";
 import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-type Props = {};
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-export default function Report({}: Props) {
-  return <div>Report</div>;
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+function getRandomNumberBetween0And100() {
+  return Math.floor(Math.random() * 101);
+}
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => getRandomNumberBetween0And100()),
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => getRandomNumberBetween0And100()),
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+export default function Report() {
+  return <Bar options={options} data={data} />;
 }
