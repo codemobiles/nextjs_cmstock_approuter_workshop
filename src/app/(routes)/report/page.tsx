23c364904f400e4d6d5bed6718319818e38651b7
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-03 21:48:01 +0700

    ep 84

--- src/app/(routes)/report/page.tsx
+++ src/app/(routes)/report/page.tsx
@@ -1,70 +1,166 @@
 "use client";
-import React from "react";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-import { Box } from "@mui/material";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import { Box, Button, ButtonGroup, IconButton, Paper } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import React, { useEffect, useState } from "react";
+import "chart.js/auto";
+import { Chart } from "react-chartjs-2";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
-export const options = {
-  responsive: true,
+function getRandomInt(): any {
+  let randoms = [];
+  for (let index = 0; index <= labels.length; index++) {
+    randoms.push(Math.floor(Math.random() * (50000 - 5 + 1)) + 5);
+  }
+  return randoms;
+}
+
+const chartOption: any = {
   plugins: {
-    legend: {
-      position: "bottom" as const,
+    tooltip: {
+      callbacks: {
+        title: function () {
+          return "CodeMobiles";
+        },
+      },
     },
+    legend: { display: true },
     title: {
       display: true,
-      text: "Chart.js Bar Chart",
+      text: "CodeMobiles ChartJS",
+      position: "top",
+    },
+  },
+  maintainAspectRatio: false,
+  scales: {
+    y: {
+      ticks: {
+        beginAtZero: true,
+        callback: function (value: any, index: any, values: any) {
+          return "฿" + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        },
+      },
     },
   },
 };
 
-function getRandomNumberBetween0And100() {
-  return Math.floor(Math.random() * 101);
-}
+const ReportPage = () => {
+  const [chartType, setChartType] = useState<string>("bar");
+  const [chartData1, setChartData1] = useState([]);
+  const [chartData2, setChartData2] = useState([]);
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+  const data: any = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Revenue 2022",
+        fill: true,
+        lineTension: 0.5, // line curve
+        backgroundColor: [
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+        ],
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 10, // circle
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: chartData1,
+      },
+      {
+        label: "Revenue 2023",
+        fill: false,
+        lineTension: 0.1, // line curve
+        borderWidth: 0.5, // line thiness
+        backgroundColor: "rgba(0, 0, 0, 0.1)",
+        borderColor: "rgba(0, 0, 0, 0.3)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(220,220,220,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 10, // circle
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(220,220,220,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: chartData2,
+      },
+    ],
+  };
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: labels.map(() => getRandomNumberBetween0And100()),
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => getRandomNumberBetween0And100()),
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-    {
-      label: "Dataset 3",
-      data: labels.map(() => getRandomNumberBetween0And100()),
-      backgroundColor: "rgba(153, 162, 235, 0.5)",
-    },
-  ],
+  useEffect(() => {
+    setChartData1(getRandomInt());
+    setChartData2(getRandomInt());
+  }, []);
+
+  return (
+    <Paper sx={{ padding: 4 }} elevation={7}>
+      <Typography variant="h1">Sale Chart</Typography>
+      <ButtonGroup
+        size="large"
+        color="primary"
+        aria-label="large outlined primary button group"
+      >
+        <Button
+          variant={chartType === "line" ? "contained" : "outlined"}
+          onClick={() => setChartType("line")}
+        >
+          Line
+        </Button>
+        <Button
+          variant={chartType === "bar" ? "contained" : "outlined"}
+          onClick={() => setChartType("bar")}
+        >
+          Bar
+        </Button>
+        <Button
+          variant={chartType === "pie" ? "contained" : "outlined"}
+          onClick={() => setChartType("pie")}
+        >
+          Pie
+        </Button>
+      </ButtonGroup>
+      <IconButton
+        aria-label="refresh"
+        onClick={() => {
+          setChartData1(getRandomInt());
+          setChartData2(getRandomInt());
+        }}
+      >
+        <RefreshIcon />
+      </IconButton>
+      <Box sx={{ height: 500 }}>
+        {chartType === "line" && (
+          <Chart type={"line"} data={data} width="100%" options={chartOption} />
+        )}
+        {chartType === "pie" && (
+          <Chart type="pie" data={data} width="100%" options={chartOption} />
+        )}
+        {chartType === "bar" && (
+          <Chart type="bar" data={data} width="100%" options={chartOption} />
+        )}
+      </Box>
+    </Paper>
+  );
 };
 
-export default function Report() {
-  return (
-    <Box className="h-[300px]">
-      <Bar options={options} data={data} />;
-    </Box>
-  );
-}
+export default ReportPage;
