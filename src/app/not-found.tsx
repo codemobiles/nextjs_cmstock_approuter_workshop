009f8a5004663bc2382676d5e34a51ef7b8eb4f9
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-05 14:05:55 +0700

    ep89

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default function NotFound() {
+  //   redirect("/");
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}
