e729755a1376f7f40d0fded77b8a9536473384ce
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-07 07:48:39 +0700

    ep94

--- src/app/(routes)/stock/edit/page.tsx
+++ src/app/(routes)/stock/edit/page.tsx
@@ -13,6 +13,8 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { doGetStockById } from "@/src/services/serverService";
 import StockEditForm from "./StockEditForm";
+import { Metadata, ResolvingMetadata } from "next";
+import { productImageURL } from "@/src/utils/commonUtil";
 
 const formValidateSchema = Yup.object().shape({
   name: Yup.string().required("Name is required").trim(),
@@ -25,6 +27,42 @@ type Props = {
     id?: string;
   };
 };
+
+export async function generateMetadata(
+  { searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  let product = {} as ProductData;
+
+  if (searchParams.id) {
+    product = await doGetStockById(searchParams.id);
+  }
+
+  return {
+    title: product.name ?? "StockEdit",
+    openGraph: {
+      title: product.name,
+      description: product.name + "price: " + product.price,
+      url: "https://nextjs.org",
+      siteName: "Next.js",
+      images: [
+        {
+          url: productImageURL(product.image),
+          width: 800,
+          height: 600,
+        },
+        {
+          url: "https://nextjs.org/og-alt.png",
+          width: 1800,
+          height: 1600,
+          alt: "My custom alt",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+  };
+}
 
 export default async function StockEdit({ searchParams }: Props) {
   let product = {} as ProductData;
