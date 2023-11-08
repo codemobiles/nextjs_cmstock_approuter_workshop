aae42ee010559bd6bcbe830c3f6a0c3710104ca7
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-08 17:19:09 +0700

    ep100

--- next.config.js
+++ next.config.js
@@ -3,7 +3,24 @@ const nextConfig = {
   output: "standalone",
   images: {
     dangerouslyAllowSVG: true,
-    domains: ["codemobiles.com", "pospos.co", "localhost"],
+    // domains: ["codemobiles.com", "pospos.co", "localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "codemobiles.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "pospos.co",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        pathname: "**",
+      },
+    ],
     minimumCacheTTL: 0,
   },
 };
